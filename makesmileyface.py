90ec370c1f814260bd9910c30b0be7b454e4bbda
ggrant24570 <gkgrant@mail.lipscomb.edu>
2024-12-09 09:21:10 -0600

    shifted image more towards middle, changed z-height to allow for cup, changed extruder speed

--- makesmileyface.py
+++ makesmileyface.py
@@ -6,10 +6,10 @@ draw_vel = 10  # s/mm
 primeDist = 200  # mm
 
 ####### Smiley Face Parameters #######
-left_eye = (-7, 20)  # (x,y)
-right_eye = (8, 20)  # (x,y)
-smile_center = (1, 11)  # (x,y)
-circle_center = (1, 11)  # (x,y)
+left_eye = (-7, -6)  # (x,y)
+right_eye = (8, -6)  # (x,y)
+smile_center = (1, -15)  # (x,y)
+circle_center = (1, -15)  # (x,y)
 
 smile_radius = 15  # mm
 circle_radius = 30  # mm
@@ -20,15 +20,15 @@ smile_arc = (206, 334) # start and stop are both 26 degrees below the horizontal
 # commands: (X, Y, Z, E command, F command)
 commands = []
 # generate priming routine
-commands += generate_priming((-40, 30, 6), prime_dist=primeDist, travel_vel=travel_vel, draw_vel=draw_vel)
+commands += generate_priming((-40, 30, 100), prime_dist=primeDist, travel_vel=travel_vel, draw_vel=draw_vel)
 cummulative_extrusion = commands[len(commands)-1][3]  # this is the correct way to access the last extrusion value in the list
 
 # generate complete circle for smiley face border
-commands += generate_arc(center=circle_center, radius=circle_radius, arc=(0, 360), angular_resolution=10, extrusion_per_mm=4.5, cummulative_extrusion=cummulative_extrusion)
+commands += generate_arc(center=circle_center, radius=circle_radius, arc=(0, 360), angular_resolution=10, extrusion_per_mm=1, cummulative_extrusion=cummulative_extrusion)
 cummulative_extrusion = commands[len(commands)-1][3]
 
 # generate arc for smile
-commands += generate_arc(center=smile_center, radius=smile_radius, arc=smile_arc, angular_resolution=10, extrusion_per_mm=4.5, cummulative_extrusion=cummulative_extrusion)
+commands += generate_arc(center=smile_center, radius=smile_radius, arc=smile_arc, angular_resolution=10, extrusion_per_mm=1, cummulative_extrusion=cummulative_extrusion)
 cummulative_extrusion = commands[len(commands)-1][3]
 
 # left eye -- using the priming function but on smaller scale to make the dots for the eyes
